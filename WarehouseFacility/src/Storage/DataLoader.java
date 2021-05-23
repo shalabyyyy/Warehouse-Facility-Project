a508c4ce19b0f420e6b5e3b71b6fee5172eff862
jamesst78 <mohadsamir@hotmail.com>
2021-05-23 18:34:54 +0200

    intial commit

---
+++ WarehouseFacility/src/Storage/DataLoader.java
@@ -0,0 +1,67 @@
+package Storage;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DataLoader {
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void loadArray(String file , ArrayList<Integer> weightArray , ArrayList<Integer> priceArray , int maxWeight)
+	{
+		int ctr = 0;
+		System.out.println(ctr);
+
+		try {
+			Scanner s1 = new Scanner(new File(file));
+			while(s1.hasNextLine())
+			{
+				ctr = ctr+1;
+				s1.nextLine();
+				//System.out.println(ctr);
+			}
+			s1.close();
+			
+			
+			
+			Scanner s2 = new Scanner(new File(file));
+			
+			//Empty the lists
+			weightArray.clear();
+			priceArray.clear();
+			
+			for(int i = 0; i<ctr ; i ++)
+			{
+				
+				
+				weightArray.add(s2.nextInt());
+				priceArray.add(s2.nextInt());
+			}
+			maxWeight = 997;
+			
+			s2.close();
+			return;
+			
+		}
+		
+		catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("Couldn't load file");
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	
+
+	}
+
+}
---
+++ WarehouseFacility/src/Storage/SubWarehouse.java
@@ -0,0 +1,108 @@
+package Storage;
+
+import java.io.Console;
+import java.util.ArrayList;
+public class SubWarehouse {
+	
+	
+	int maxCapacity;
+	ArrayList<Integer> itemWeights = new ArrayList<>() ;
+	ArrayList<Integer> itemProfits = new ArrayList<>(); 					
+	int maxChosenProfit;
+	int availableItems;
+	
+	
+	
+	
+	
+	public void optimizeSelection()
+	{
+		//TODO: Create a method that optimizes the selection of items in order to produce the most profit and make the most out of the storage space.
+		
+		//Your code here
+		//You're allowed to use any other pre-defined methods.
+		
+		
+	
+        
+ 
+        maxChosenProfit =  0; //Update with your answer.
+    }
+		
+	
+	
+	public static int max(int a, int b)
+	    {
+	      return (a > b) ? a : b;
+	    }
+	
+	
+	
+	
+
+	public static void main(String[] args) {
+		
+		SubWarehouse SW = new SubWarehouse();
+		
+		//populating the ArrayLists   
+		SW.itemWeights.add(56);
+		SW.itemWeights.add(59);
+		SW.itemWeights.add(80);       // itemWeights = {56 , 59 , 80 , 64, 75 , 17};
+		SW.itemWeights.add(64);
+		SW.itemWeights.add(75);
+		SW.itemWeights.add(17);
+		
+		
+		SW.itemProfits.add(50);                                
+		SW.itemProfits.add(50);
+		SW.itemProfits.add(64);
+		SW.itemProfits.add(46);		  // itemProfits = {50 , 50 , 64 , 46 , 50 , 5}; 
+		SW.itemProfits.add(50);
+		SW.itemProfits.add(5);
+
+		SW.maxCapacity = 190;          	   //maximum warehouse capacity = 190 weight unit
+		SW.availableItems = 6; 			   //available items are 6
+		
+		
+		
+		
+		
+		//Step 1 :
+		//TODO: Call your optimization method here and Print your results.
+		//Expected Results : Populate maxChosenProfit with the total final Profit
+		
+		SW.optimizeSelection();
+		System.out.println( "Max Profit " + SW.maxChosenProfit); //optimum answer should be maxChosenProfit = 150
+
+		
+		
+		
+		//Step 2 :
+		//TODO: Try out your solution on more complex datasets that are used in practical world
+		
+		
+		//Uncomment to load the dataset before re-running your method again
+		
+		
+		
+		/*DataLoader.loadArray("kp100.txt", SW.itemWeights, SW.itemProfits, SW.maxCapacity);
+		 * 
+		SW.maxCapacity= 49877;
+		SW.availableItems = 10000 ;
+		SW.optimizeSelection();
+		System.out.println( "Max Profit " + SW.maxChosenProfit); // should be Max Profit 594669   
+		*
+		*
+		*/
+
+
+		
+		
+		
+		
+		
+	}
+
+	
+	
+}
